8b124965c740ab78bf4a9884649011d986842123
Atsushi Kokubo <kokubo@aomori-u.ac.jp>
2013-10-01 15:16:31 +0900

    membersコントローラー中のMemberの属性の誤りを訂正

--- app/controllers/members_controller.rb
+++ app/controllers/members_controller.rb
@@ -28,7 +28,7 @@ class MembersController < ApplicationController
 
     respond_to do |format|
       if @member.save
-        format.html { redirect_to @member, notice: 'Member:#{@member.name} was successfully created.' }
+        format.html { redirect_to @member, notice: 'Member:#{@member.user} was successfully created.' }
         format.json { render action: 'show', status: :created, location: @member }
       else
         format.html { render action: 'new' }
@@ -42,7 +42,7 @@ class MembersController < ApplicationController
   def update
     respond_to do |format|
       if @member.update(member_params)
-        format.html { redirect_to @member, notice: 'Member:#{@member.name} was successfully updated.' }
+        format.html { redirect_to @member, notice: 'Member:#{@member.user} was successfully updated.' }
         format.json { head :no_content }
       else
         format.html { render action: 'edit' }
