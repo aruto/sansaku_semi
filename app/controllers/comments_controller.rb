410ab81add3a08176024b96ea20874b263df4c81
Masashi-oda <sailnismoda@gmail.com>
2013-12-05 18:01:59 +0900

    コメント修正

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,6 +1,6 @@
 class CommentsController < ApplicationController
 	def create
-		place = Place.find params[:places_id]
+		place = Place.find params[:place_id]
 		place.comments.create params[:comment]
 		redirect_to place
 	end
