6b2f395ebd5987999795f887944ee36668ba14d8
Hokuto Kon <bei_north@yahoo.co.jp>
2013-12-09 12:38:01 +0900

    ソートのセーブ途中

--- app/controllers/my_maps_controller.rb
+++ app/controllers/my_maps_controller.rb
@@ -5,8 +5,8 @@ class MyMapsController < ApplicationController
   # GET /my_maps.json
   def index
     @way_points = @current_member.my_map.way_points
-@my_map = @current_member.my_map
-render layout: "my_maps"
+    @my_map = @current_member.my_map
+    render layout: "my_maps"
   end
 
   # GET /my_maps/1
